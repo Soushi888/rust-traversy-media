81b1ba91c36714f221b1518ef8775a97fce014ec
Soushi888 <Sacha.pignot@protonmail.com>
2021-05-22 01:15:31 -0400

    tuples.rs

---
+++ src/tuples.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    // tableau de types
+    let person1: (&str, &str, i8) = ("Sacha", "Longueuil", 28);
+    let person2 = ("Josiane", "Saint-Honoré", 27);
+
+    println!("{} is from {} et is {} years old.", person1.0, person1.1, person1.2 );
+    println!("{} is from {} et is {} years old.", person2.0, person2.1, person2.2 );
+}

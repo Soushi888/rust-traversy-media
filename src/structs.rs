2146db88d4cc0ee83ae02ff8e7490163b51a6e93
Soushi888 <Sacha.pignot@protonmail.com>
2021-05-24 20:04:27 -0400

    Calculator Struct

--- src/structs.rs
+++ src/structs.rs
@@ -1,14 +1,23 @@
 // Structs - Used to create custom data types
 
 // Traditional Struct
-// struct Color {
-//     red: u8,
-//     green: u8,
-//     blue: u8,
-// }
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
 // Tuple Struct
-// struct Color(u8, u8, u8);
+struct ColorTuple(u8, u8, u8);
+
+struct Calculator;
+
+impl Calculator {
+    fn add(n1: i32, n2: i32) -> i32 { n1 + n2 }
+    fn sub(n1: i32, n2: i32) -> i32 { n1 - n2 }
+    fn mul(n1: i32, n2: i32) -> i32 { n1 * n2 }
+    fn div(n1: i32, n2: i32) -> i32 { n1 / n2 }
+}
 
 struct Person {
     first_name: String,
@@ -42,17 +51,22 @@ impl Person {
 
 
 pub fn run() {
-    // let mut c = Color {
-    //     red: 255,
-    //     green: 0,
-    //     blue: 0,
-    // };
-    // c.red = 200;
-    // println!("color: {}, {}, {}", c.red, c.green, c.blue);
+    let mut c = Color {
+        red: 255,
+        green: 0,
+        blue: 0,
+    };
+    c.red = 200;
+    println!("color: {}, {}, {}", c.red, c.green, c.blue);
 
-    // let mut c = Color(200, 0 ,0);
-    // c.0 = 220;
-    // println!("color: {}, {}, {}", c.0, c.1, c.2)
+    let mut c = ColorTuple(200, 0, 0);
+    c.0 = 220;
+    println!("color: {}, {}, {}", c.0, c.1, c.2);
+
+    println!("{}", Calculator::add(2, 3));
+    println!("{}", Calculator::sub(2, 3));
+    println!("{}", Calculator::mul(2, 3));
+    println!("{}", Calculator::div(30, 3));
 
     let mut p = Person::new("Sacha", "Pignot");
     println!("Person : {}", p.full_name());
